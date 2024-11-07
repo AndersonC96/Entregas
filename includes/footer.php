ecef677df035db281f25d19ce938a9876c194d83
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-11-07 12:36:09 -0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
+        <div class="footer-text text-center mt-3" style="font-size: 14px; color: #6c757d;">
+            &copy; <?= date("Y") ?> Simple Pharma
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <footer class="text-center mt-4 mb-4">
-            <p>&copy; <?= date("Y") ?> Simple Pharma</p>
-        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-p5yTVzASd07w5A1mPtfjPsLuNG7ly8Vq91MRsLfAynvU31viFyw7kKZ05LBiCZ0Q" crossorigin="anonymous"></script>
     </body>
 </html>
