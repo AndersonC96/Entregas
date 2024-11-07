c0e63a46fe0b910aee6d0a2ac351d69ad309ce79
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-11-07 17:58:17 -0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,33 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="dashboard.php">Sistema de Entregas</a>
+        <a class="navbar-brand" href="../dashboard.php">Sistema de Entregas</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="reqs/view_reqs.php">Visualizar Requisições</a>
+                    <a class="nav-link" href="view_reqs.php">Visualizar Requisições</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="reqs/add_req.php">Cadastrar Requisição</a>
+                    <a class="nav-link" href="add_req.php">Cadastrar Requisição</a>
                 </li>
                 <?php if (isset($_SESSION['user']) && $_SESSION['user']['is_admin']): ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="admin/create_user.php">Gerenciar Usuários</a>
+                        <a class="nav-link" href="../admin/create_user.php">Gerenciar Usuários</a>
                     </li>
                 <?php endif; ?>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
             </ul>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
