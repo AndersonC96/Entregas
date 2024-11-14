760d4cede5d8761471ff7e607d1d914e3e4a39bf
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-11-14 15:47:20 -0300

    Update report.php

--- reqs/report.php
+++ reqs/report.php
@@ -13,18 +13,18 @@
     <h2 class="text-center mb-4">Gerar Relatório de Requisições</h2>
     <form action="process_report.php" method="GET" class="p-4" style="background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
         <div class="mb-3">
-            <label for="data_inicio" class="form-label">Data Início</label>
+            <label for="data_inicio" class="form-label"><b>Data Início</b></label>
             <input type="date" class="form-control" id="data_inicio" name="data_inicio" value="">
         </div>
         <div class="mb-3">
-            <label for="data_fim" class="form-label">Data Fim</label>
+            <label for="data_fim" class="form-label"><b>Data Fim</b></label>
             <input type="date" class="form-control" id="data_fim" name="data_fim" value="">
         </div>
         <div class="mb-3">
-            <label for="entregador" class="form-label">Entregador</label>
+            <label for="entregador" class="form-label"><b>Entregador</b></label>
             <input type="text" class="form-control" id="entregador" name="entregador" placeholder="Nome do Entregador" value="">
         </div>
-        <button type="submit" class="btn btn-primary w-100" style="background-color: #52b1a9; border: none;">Gerar Relatório</button>
+        <button type="submit" class="btn btn-primary w-100" style="background-color: #52b1a9; border: none;"><b>Gerar Relatório</b></button>
     </form>
 </div>
 <?php include '../includes/footer.php'; ?>
